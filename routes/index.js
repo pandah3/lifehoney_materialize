e982700b3367252370e4dafdc72a39c561da559d
pandah3 <jlee.78@live.com>
2018-08-30 00:37:42 -0700

    add jquery

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // rendering index.hbs
-  res.render('index', { title: 'Express' });
+  // rendering index.hbs & title in head of layout.hbs (for tab title)
+  res.render('index', { title: 'LIFE HONEY' });
 });
 
 module.exports = router;
