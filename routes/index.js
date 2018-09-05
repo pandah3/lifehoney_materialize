319f2458431b956a6b5d8d9a8f09a555deda8564
pandah3 <jlee.78@live.com>
2018-09-04 19:59:12 -0700

    add items

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // rendering index.hbs & title in head of layout.hbs (for tab title)
-  res.render('index', { title: 'LIFE HONEY' });
+  res.render('shop/index', { title: 'LIFE HONEY' });
 });
 
 module.exports = router;
